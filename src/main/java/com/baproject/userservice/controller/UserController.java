8f0bd97512a3411675d861e556f89b2fa33dabbf
bademba <brian.ademba@gmail.com>
2023-06-18 22:15:22 +0300

    updated responses & fixed PUT method

--- src/main/java/com/baproject/userservice/controller/UserController.java
+++ src/main/java/com/baproject/userservice/controller/UserController.java
@@ -28,13 +28,13 @@ public class UserController {
     String timestamp = DateFor.format(date);
 
     @GetMapping("/users")
-    public ResponseEntity<List<User>> users(){
-        return new ResponseEntity(userService.users(), HttpStatus.OK);
+    public ResponseEntity<Object> users(){
+        return ResponseHandler.generateResponse(uuid,"Users successfully retrieved",HttpStatus.OK,userService.users(),timestamp);
     }
 
     @GetMapping("/users/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-         return new ResponseEntity(userService.get(id),HttpStatus.OK);
+    public ResponseEntity<Object> findById(@PathVariable Integer id){
+        return ResponseHandler.generateResponse(uuid,"User details retrieved successfully",HttpStatus.OK,userService.get(id),timestamp);
     }
     @PostMapping("/users/newUser")
     public ResponseEntity saveUser(@RequestBody User user){
@@ -43,14 +43,18 @@ public class UserController {
 
     @PutMapping("/users/{id}")
     public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable Integer id){
-        try{
-            User userEXist =userService.get(id);
-            userService.saveUser(user);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }catch(NoSuchElementException e){
-            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        User currentUser=userService.get(id);
+        if(currentUser ==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        currentUser.setFirstName(user.getFirstName());
+        currentUser.setMiddleName(user.getMiddleName());
+        currentUser.setLastName(user.getLastName());
+
+        User updatedUser =userService.saveUser(currentUser);
+        return ResponseHandler.generateResponse(uuid,"User details updated successfully",HttpStatus.OK,updatedUser,timestamp);
     }
+
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable Integer id){
         userService.delete(id);
