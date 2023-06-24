ac3ad87a20cfd79eeeecf5c3e6e149e063f1f465
bademba <brian.ademba@gmail.com>
2023-06-24 21:36:39 +0300

    Adding logger

--- src/main/java/com/baproject/userservice/controller/UserController.java
+++ src/main/java/com/baproject/userservice/controller/UserController.java
@@ -4,6 +4,8 @@ import com.baproject.userservice.entity.User;
 import com.baproject.userservice.repository.UserRepository;
 import com.baproject.userservice.response.ResponseHandler;
 import com.baproject.userservice.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
