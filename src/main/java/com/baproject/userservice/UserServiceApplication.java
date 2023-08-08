4c81d97e61b6b0b6684a7565c33fb5798c9d9ae6
bademba <brian.ademba@gmail.com>
2023-08-08 19:15:14 +0300

    Added Eureka client discovery

--- src/main/java/com/baproject/userservice/UserServiceApplication.java
+++ src/main/java/com/baproject/userservice/UserServiceApplication.java
@@ -2,10 +2,12 @@ package com.baproject.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.baproject.userservice.repository")
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
